3542fef5a33c4c5539c79c95e307a4c3a9697759
hkijin <99144736+hkijin@users.noreply.github.com>
2022-08-27 17:15:49 +0900

    v0.5.5  無駄合判定　角を除外

--- source/shtsume/ntbase.c
+++ source/shtsume/ntbase.c
@@ -820,7 +820,7 @@ bool hs_invalid_drops      (const sdata_t *sdata,
 {
     komainf_t koma = S_BOARD(sdata, src);
     //角の王手には適用しない
-    //if(koma == SKA||koma==GKA) return false;
+    if(koma == SKA||koma==GKA) return false;
     //if(koma != SRY && koma!=GRY) return false;
     sdata_t sbuf, sbuf1;
     bool flag = false; //true 駒がdestの位置で成れる。
--- source/shtsume/shtsume.h
+++ source/shtsume/shtsume.h
@@ -20,7 +20,7 @@
  * プログラムID
  */
 #define PROGRAM_NAME       "shtsume"
-#define VERSION_INFO       "v0.5.4"
+#define VERSION_INFO       "v0.5.5"
 #define AUTHOR_NAME        "hkijin"
 
 /*
