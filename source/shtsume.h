39d68e82ea12be431fb7ae0070a743b0380351b4
hkijin <99144736+hkijin@users.noreply.github.com>
2023-02-09 12:00:35 +0900

    v1.0.12 にバージョンアップ。

--- source/shtsume.h
+++ source/shtsume.h
@@ -20,7 +20,7 @@
  * プログラムID
  */
 #define PROGRAM_NAME       "shtsume"
-#define VERSION_INFO       "v1.0.11"
+#define VERSION_INFO       "v1.0.12"
 #define AUTHOR_NAME        "hkijin"
 /*
  * USIオプション情報
