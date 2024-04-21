0b0c7535807a0063fa0545aeadc36d633edec4ba
hkijin <99144736+hkijin@users.noreply.github.com>
2024-04-21 16:06:02 +0900

    v1.2.0 USI設定メニューの表示変更　 余詰探索レベル→探索レベル、　余詰探索時の棋譜出力→探索レベル毎の棋譜出力 以下の関連ファイル修正 shtsume.h, shtsume_ja, README, release_note

--- source/shtsume.h
+++ source/shtsume.h
@@ -20,7 +20,7 @@
  * プログラムID
  */
 #define PROGRAM_NAME       "shtsume"
-#define VERSION_INFO       "v1.1.8"
+#define VERSION_INFO       "v1.2.0"
 #define AUTHOR_NAME        "hkijin"
 /*
  * USIオプション情報
